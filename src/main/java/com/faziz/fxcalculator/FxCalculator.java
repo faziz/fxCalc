ef3abcfa3f7ab3434487253cf4189ffa2c328a88
faisal <faisal@kaamwala>
2017-06-24 08:37:12 +0500

    Base Spring boot setup.

---
+++ src/main/java/com/faziz/fxcalculator/FxCalculator.java
@@ -0,0 +1,12 @@
+package com.faziz.fxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxCalculator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxCalculator.class, args);
+    }
+}
